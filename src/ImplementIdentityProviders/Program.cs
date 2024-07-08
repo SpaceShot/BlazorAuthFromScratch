06a89b3b7f9cc9deccfadc837714ad6501797b4f
Chris Gomez <cagomez@cellbycell.com>
2024-07-07 21:51:52 -0400

    blah

---
+++ src/ImplementIdentityProviders/Program.cs
@@ -0,0 +1,38 @@
+using BlazorAuthFromScratchServer.Data;
+using ImplementIdentityProviders.Components;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
+    .AddIdentityCookies();
+
+//builder.Services.AddIdentityCore<ApplicationUser>()
+//    .AddSignInManager()
+//    .AddDefaultTokenProviders();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
